8530bb0f0cbbc4381ca433c08e7d85ec13b8e4bd
Roy Touw <roytouw@hotmail.com>
2020-03-26 09:15:06 +0100

    Removed depricated code.

--- src/content.js
+++ src/content.js
@@ -8,46 +8,8 @@ export const operator$ = new Observable();
 export const reset$ = new Observable();
 export let subscription$;
 
-// const backpageMessageSubject$ = new Subject();
-// backpageMessageSubject$.subscribe(x => console.log(x));
-// const timer$ = interval(1000);
-// const test$ = new Subject();
-// test$.subscribe(x => console.log(x));
-// const backpageMessageSubject$ = new Subject().pipe(tap(x => console.log(x)));
-// const backpageMessageSubjectEmiter$ = zip(backpageMessageSubject$, timer$).pipe(
-//     map(([value, _]) => value)
-// );
-
-// const createMessageObservable = (source$, messageType) => {
-//     return source$.pipe(
-//         filter(message => message.type === messageType),
-//         pluck('message')
-//     );
-// };
-
 export let print = msg => console.log(msg); // For development purpose.
-// export const observable$ = createMessageObservable(backpageMessageSubjectEmiter$, 'observable');
-// export const observable$ = new Observable(subscriber => {
-//     createMessageObservable(backpageMessageSubject$, 'observable').subscribe(msg => {
-//         console.log('this is happening');
-//         subscriber.next(msg);
-//     });
-// });
-// export const operator$ = createMessageObservable(backpageMessageSubjectEmiter$, 'operator');
-// export const event$ = createMessageObservable(backpageMessageSubjectEmiter$, 'event');
-// export const reset$ = backpageMessageSubject$.pipe(
-//     filter(message => message.type === 'reset'),
-// );
 
-// // TODO: curriedProducer is undefined at return.
-// const createObservableWithProducer = () => {
-//     let curriedProducer;
-//     const producer = subscriber => value => subscriber.next(value);
-//     const observable = new Observable(subscriber => {
-//         curriedProducer = producer(subscriber);
-//     });
-//     return [observable, curriedProducer];
-// };
 
 const subjects = new Map();
 const dispatchMessage = (message, subscriber) => {
@@ -95,13 +57,7 @@ if (!development) {
     // TODO: each non-observable message must be sent in new observables.
     subscription$ = new Observable(subscriber => {
         backgroundPageConnection.onMessage.addListener(msg => {
-            // backpageMessageSubject$.next(msg);
-            // subscriber.next(msg);
             dispatchMessage(msg, subscriber);
-            // if (msg.type === 'operator') {
-            //     const { type, line, observable } = msg.message;
-            //     print(`type: ${type} on ${line} with observable ${observable} `);
-            // }
         });
     });
 
@@ -120,42 +76,4 @@ if (!development) {
         dispatchMessage(getObservableMock(), subscriber);
         dispatchMessage(eventMock.pop(), subscriber);
     });
-
-    // // TODO: are they the right observables or too many?
-    // subscription$.subscribe(message => {
-    //     let counter = 0;
-    //     const [observable, observable$] = message;
-    //     observable$.subscribe(innerMessage => {
-    //         console.log(`inner message ${innerMessage} in subscription ${++counter}`);
-    //     });
-    // });
-
-    // observable$ = new Observable(subsriber => {
-    //     subsriber.next(observableMock);
-    // });
-    // test$.next(1);
-    // setTimeout(() => {
-    //     backpageMessageSubject$.next(observableMock);
-    // }, 100);
-    // setTimeout(() => {
-    //     backpageMessageSubject$.next(observable);
-    //     backpageMessageSubject$.next(observable);
-    // }, 100);
-    // setTimeout(() => {
-    //     for (let i = 0; i < 10; i++) {
-    //         const observable = getObservableMock();
-    //         backpageMessageSubject$.next(observable);
-    //         setTimeout(() => {
-    //             [1, 2, 3].map(() => backpageMessageSubject$.next(getOperatorMock(observable.message.uuid)));
-    //             [1, 2, 3, 4].map(() => {
-    //                 getEventMock(observable.message.uuid, 4).map(event => backpageMessageSubject$.next(event));
-    //             });
-    //         }, 100);
-    //     }
-    // }, 100);
-
-    // setTimeout(() => {
-    //     backpageMessageSubject$.next({ type: 'reset' })
-    // }, 5000);
-
 }
