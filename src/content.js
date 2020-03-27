948ce8c159d1833e8c45b6f2e134196781320e68
Roy Touw <roytouw@hotmail.com>
2020-03-27 08:38:05 +0100

    Removed deprecated code.

--- src/content.js
+++ src/content.js
@@ -11,9 +11,12 @@ const mockMessages = () => {
     });
 };
 
+let test = function() {
+
+}
 const development = true;
 export const getMessage$ = () => {
-    if (development)
+    if (development)    // Outside extension there is no chrome object.
         return new Promise(resolve => resolve(mockMessages()));
 
     declare var chrome; // Declaration required to satisfy compiler, filled by chrome.
@@ -32,63 +35,3 @@ export const getMessage$ = () => {
         );
     });
 };
-
-// const timer$ = interval(10);
-// const backpageMessageSubject$ = new Subject();
-// const backpageMessageSubjectEmiter$ = zip(backpageMessageSubject$, timer$).pipe(
-//     map(([value, _]) => value)
-// );
-
-// const createMessageObservable = (source$, messageType) => {
-//     return source$.pipe(
-//         filter(message => message.type === messageType),
-//         pluck('message')
-//     );
-// };
-
-// export let print = msg => console.log(msg); // For development purpose.
-// export const observable$ = createMessageObservable(backpageMessageSubjectEmiter$, 'observable');
-// export const operator$ = createMessageObservable(backpageMessageSubjectEmiter$, 'operator');
-// export const event$ = createMessageObservable(backpageMessageSubjectEmiter$, 'event');
-// export const reset$ = backpageMessageSubject$.pipe(
-//     filter(message => message.type === 'reset'),
-// );
-
-// const development = true;
-// if (!development) {
-//     declare var chrome;
-
-//     print = msg => chrome.extension.getBackgroundPage().console.log(msg);
-
-//     const backgroundPageConnection = chrome.runtime.connect({
-//         name: 'messageListener'
-//     });
-
-
-//     backgroundPageConnection.onMessage.addListener(msg => {
-//         backpageMessageSubject$.next(msg);
-//         if (msg.type === 'operator') {
-//             const { type, line, observable } = msg.message;
-//             print(`type: ${type} on ${line} with observable ${observable} `);
-//         }
-//     });
-
-// } else {    // Development Block.
-//     setTimeout(() => {
-//         for (let i = 0; i < 10; i++) {
-//             const observable = getObservableMock();
-//             backpageMessageSubject$.next(observable);
-//             setTimeout(() => {
-//                 [1, 2, 3].map(() => backpageMessageSubject$.next(getOperatorMock(observable.message.uuid)));
-//                 [1, 2, 3, 4].map(() => {
-//                     getEventMock(observable.message.uuid, 4).map(event => backpageMessageSubject$.next(event));
-//                 });
-//             }, 100);
-//         }
-//     }, 100);
-
-//     setTimeout(() => {
-//         backpageMessageSubject$.next({ type: 'reset' })
-//     }, 5000);
-
-// }
